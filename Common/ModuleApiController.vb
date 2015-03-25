e6b2c44c0fb928bbb8f6817dea74f31b7acc1555
Peter Donker <peter@bring2mind.net>
2015-03-25 13:29:04 +0100

    Implement security in the services layer

---
+++ Common/ModuleApiController.vb
@@ -0,0 +1,19 @@
+﻿Imports DotNetNuke.Entities.Users
+Imports DotNetNuke.Web.Api
+
+Namespace Common
+ Public Class ModuleApiController
+  Inherits DnnApiController
+
+  Private _security As ContextSecurity
+  Public ReadOnly Property Security As ContextSecurity
+   Get
+    If _security Is Nothing Then
+     _security = New ContextSecurity(ActiveModule, UserController.GetCurrentUserInfo())
+    End If
+    Return _security
+   End Get
+  End Property
+
+ End Class
+End Namespace
